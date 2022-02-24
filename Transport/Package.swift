5d80a02102ba0da1a7a10d4791911aca4412c5de
gookhee <gookhee@wantedlab.com>
2022-02-24 10:10:07 +0900

    fix: 이미지 리소스 위치 옮김
    - 추가로 빌드 에러 수정

--- Transport/Package.swift
+++ Transport/Package.swift
@@ -16,6 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "ModernRIBs", url: "https://github.com/DevYeom/ModernRIBs", .exact("1.0.1")),
+        .package(name: "Finance", path: "../Finance")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,6 +25,13 @@ let package = Package(
             name: "TransportHome",
             dependencies: [
                 "ModernRIBs",
-            ]),
+                .product(name: "FinanceRepository", package: "Finance"),
+                .product(name: "Topup", package: "Finance")
+            ],
+            resources: [
+                .process("Resource")
+            ]
+        ),
+        
     ]
 )
