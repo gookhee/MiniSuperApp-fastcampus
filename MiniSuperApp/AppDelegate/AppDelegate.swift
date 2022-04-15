81104a10a3ea7f479fa59b95ed30963c0c02b598
gookhee <62234564+gookhee@users.noreply.github.com>
2022-04-15 18:53:45 +0900

    style: 필요없는 코드 제거

--- MiniSuperApp/AppDelegate/AppDelegate.swift
+++ MiniSuperApp/AppDelegate/AppDelegate.swift
@@ -6,9 +6,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    private var launchRouter: LaunchRouting?
-    private var urlHandler: URLHandler?
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let window = UIWindow(frame: UIScreen.main.bounds)
@@ -22,8 +19,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-}
-
-protocol URLHandler: AnyObject {
-    func handle(_ url: URL)
 }
