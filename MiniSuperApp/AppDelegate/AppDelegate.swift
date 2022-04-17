8e7434c04b7d44591004185317892d98474a63a9
gookhee <62234564+gookhee@users.noreply.github.com>
2022-04-17 21:03:22 +0900

    feat: 의존성 주입 시작하기 전에 registerProviderFactories() 호출 추가

--- MiniSuperApp/AppDelegate/AppDelegate.swift
+++ MiniSuperApp/AppDelegate/AppDelegate.swift
@@ -2,10 +2,10 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        registerProviderFactories()
         
         let window = UIWindow(frame: UIScreen.main.bounds)
         self.window = window
